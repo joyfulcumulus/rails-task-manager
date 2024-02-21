99c7bbfd954c4eb991f0ee3b87317ecaa0a2b4c7
YY T <changiyy07@gmail.com>
2024-02-21 20:11:57 +0800

    submit task manager app

--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,13 @@ Rails.application.routes.draw do
   get "up" => "rails/health#show", as: :rails_health_check
 
   # Defines the root path route ("/")
-  # root "posts#index"
+  root "tasks#index"
+
+  get 'tasks', to: 'tasks#index'
+  get 'tasks/new', to: 'tasks#new'
+  get 'tasks/:id', to: 'tasks#show', as: :task
+  post 'tasks', to: 'tasks#create'
+  get 'tasks/:id/edit', to: 'tasks#edit', as: :task_edit
+  patch 'tasks/:id', to: 'tasks#update', as: :task_update
+  delete 'tasks/:id', to: 'tasks#destroy'
 end
